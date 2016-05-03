2e34ad881e18ec42d1e75712d8cd41c739bc1e7b
‘K.C.Saff’ <kc@saff.net>
2016-05-03 17:32:28 -0500

    Version that works from the command line.

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+from pip.download import PipSession
+
+version = '0.1.0'
+
+def read(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
+
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(name='filks',
+      version=version,
+      description=('Generates filks based on a poem format and corpus'),
+      long_description='\n\n'.join((read('README.md'), read('CHANGELOG'))),
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Intended Audience :: Other Audience',
+          'Programming Language :: Python :: 3'],
+      author='K.C.Saff',
+      author_email='kc@saff.net',
+      url='https://github.com/kcsaff/filks',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=reqs,
+      entry_points={
+          'console_scripts': ['filk = filks:main']
+      },
+      include_package_data = True,
+      package_data={'filks.resources.cmudict': ['*']},
+      # data_files=[
+      #     ('config', ['povray.ini'])
+      # ]
+)
